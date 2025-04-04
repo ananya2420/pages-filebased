5ee03d284788042fefb41778b973a29458a0c83e
ananya2420 <114337926+ananya2420@users.noreply.github.com>
2025-04-04 17:59:47 +0600

    Add files via upload

---
+++ list.js
@@ -0,0 +1,7 @@
+function ListPage(){
+    return <div>
+        <h1>The List page</h1>
+
+        </div>
+}
+export default ListPage;
---
+++ page.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
+
+  return (
+    <div>
+      <h2>The Clients Project Page</h2>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/Clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
